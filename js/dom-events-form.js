f2c100f0930c692565917827b2ac6476343cbde6
Erpreet <preetkaurchd15@gmail.com>
2020-08-24 12:08:37 -0600

    form element event occured on

--- js/dom-events-form.js
+++ js/dom-events-form.js
@@ -1,3 +1,16 @@
 
 const helloForm = document.querySelector('form');
 console.log(helloForm);
+
+const nameField = document.getElementById('your-name');
+console.log(nameField);
+
+const helloList = document.querySelector('#hello-list');
+console.log(helloList);
+
+helloForm.addEventListener('submit',(event ) => {
+    event.preventDefault();
+    console.log(event);
+    console.log(event.target);
+
+});
