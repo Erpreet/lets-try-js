e127dbdd0832c7e43ab7c35df4051338a7d945b0
Erpreet <preetkaurchd15@gmail.com>
2020-08-25 10:58:26 -0600

    Object Constructor

--- js/oop.js
+++ js/oop.js
@@ -1,4 +1,4 @@
-alert("JS getting loaded!");
+
 
 const myObj = {
     name: "Joe",
@@ -8,12 +8,57 @@ const myObj = {
 
 console.log(`My name is ${myObj.name}. I am ${myObj.age} years old!`);
 
+console.log(myObj);
+
+const myObjString = JSON.stringify(myObj);
+console.log(myObjString);
+
 const secondPersonString = `{
     "name":"Preet",
     "age":29,
     "hobbies":["Deep Sea Fishing","Cycling"]
 }`;
-console.log(secondPersonString.name);
+console.log(secondPersonString);
 
 const secondPersonObj = JSON.parse(secondPersonString);
 console.log(secondPersonObj);
+
+class Person
+{
+    contructor (name = "", age=0, hobbies=[])
+    {
+        this.name = name;
+        this.age = Number(age);
+        this.hobbies = hobbies;
+    }
+
+    sayHello()
+    {
+        const helloP = document.createElement("P");
+        helloP.textContent = `Hello there! I am ${this.name},nice to meet you!`;
+        document.body.appendChild(helloP);
+    }
+}
+
+const jane = new Person (
+    "Jane",
+    34,
+    ["Sewing", "Cross-Country Skiing"]
+    );
+
+    console.log(jane);
+    console.log(jane instanceof Person);
+
+    jane.sayHello();
+
+    const dimitri = new Person(
+        "Dimitri",
+        41,
+        ["Painting", "Driving"]
+    );
+
+    console.log(dimitri);
+
+    console.log(`My name is ${dimitri.name}. I am ${dimitri.age} years old`);
+
+    dimitri.sayHello();
