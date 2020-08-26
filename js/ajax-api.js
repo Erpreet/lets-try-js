03c2d6f59bff90467980117cf9122f154f5ad91f
Erpreet <preetkaurchd15@gmail.com>
2020-08-26 12:04:46 -0600

    Timer Added

--- js/ajax-api.js
+++ js/ajax-api.js
@@ -25,3 +25,35 @@ fetch('http://api.open-notify.org/astros.json') //Add fetch to retreive Astronau
 {
     console.log(error); // Good idea to output when debugging
 } );
+
+
+/**
+ * Lets grab live data based on timer
+ */
+
+ const latDD = document.getElementById('lat');
+ const longDD = document.getElementById('long');
+ console.log(latDD);
+ console.log(longDD); // Test to make sure right stuff was grabbed
+
+
+ //Add a function that grabs data from API and updates the element
+const updateLatLong = () => {
+    fetch('http://api.open-notify.org/iss-now.json')
+    .then(response => {return response.json();}) //return a js object converted from JSON response string
+    .then(data => {
+        console.log(data); // check if data came thru
+        //Grab our latitude n longitude from data object
+        const lat = data.iss_position.latitude;
+        const long = data.iss_position.longitude;
+
+        latDD.textContent=lat;
+        longDD.textContent=long;
+    })
+    .catch(error => {console.log(error); });
+}
+updateLatLong();
+ //Add a repeating timer so that function can repeat
+
+ setInterval(updateLatLong, 1500);  //repeat "updateLatLong" every 15 sec-1500 ms
+
